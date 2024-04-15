f9f2543d67216069b85942fc76d78d5614557fbd
Navneet sharma <navneetsharma9340@gmail.com>
2024-04-15 14:30:07 +0530

    last

--- src/components/auth/route.js
+++ src/components/auth/route.js
@@ -18,7 +18,7 @@ const Routers = () => {
             <Routes>
                 <Route path="/" element={<SignUp />}></Route>
                 <Route path="https://todolist-with-firebase.onrender.com/login" element={loggedInUser ? (<Navigate to="https://todolist-with-firebase.onrender.com/todo-list" />) : (<Login />)}></Route>
-                <Route path="https://todolist-with-firebase.onrender.com/login/todo-list" element={loggedInUser ? <ToDoList /> : <Navigate to="https://todolist-with-firebase.onrender.com/login" />}
+                <Route path="https://todolist-with-firebase.onrender.com/todo-list" element={loggedInUser ? <ToDoList /> : <Navigate to="https://todolist-with-firebase.onrender.com/login" />}
                 />
             </Routes>
         </>
